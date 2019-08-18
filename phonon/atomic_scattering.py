cf73ecfea7951b68e6f792228502c69d0d4b2c2b
Jiao Lin <linjiao@ornl.gov>
2019-08-18 11:37:00 -0400

    Refs #13. added AtomicSatterer class

---
+++ phonon/atomic_scattering.py
@@ -0,0 +1,43 @@
+# -*- Python -*-
+
+import numpy as np
+import periodictable as pt
+
+
+class AtomicScattering:
+    
+    """This class gather methods related to calculation of scattering
+    property of an element.
+    """
+
+    def __init__(self, element, isotope=None):
+        """AtomicScattering("H", 2)
+        AtomicScattering("H")
+        """
+        element = ''.join(c for c in element if c.isalpha()) # remove numbers
+        self.element = element
+        ptel = getattr(pt, element)
+        if isotope is not None:
+            ns = ptel[isotope].neutron
+        else:
+            ns = ptel.neutron
+        self.ns = ns
+        return
+
+
+    def b(self):
+        "bound scattering length"
+        return self.ns.b_c
+
+
+    def sigma_inc(self):
+        "incoherent scattering cross section"
+        return self.ns.incoherent
+
+
+    def sigma_abs(self):
+        "absorption cross section"
+        return self.ns.absorption
+
+
+# End of file
