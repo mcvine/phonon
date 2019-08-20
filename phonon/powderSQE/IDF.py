corrected normalization etc. see notes

phonon/powderSQE/IDF.py
@@ -30,7 +30,6 @@ def from_data_dir(
         disp = None,
         N = int(1e6),
         Q_bins = np.arange(0, 11, 0.1), E_bins = np.arange(0, 50, 0.5),
-        mass = 12., # "average" mass
         doshist=None,
         T=300., Ei=100., max_det_angle=135.,
         include_multiphonon=True,
@@ -69,6 +68,13 @@ def from_data_dir(
     from phonopy.interface import vasp
     atoms = vasp.read_vasp(poscar)
     positions = atoms.get_scaled_positions()
+    masses = atoms.get_masses()
+    average_mass = np.mean(masses)
+    symbols = atoms.get_chemical_symbols()
+    from ..atomic_scattering import AtomicScattering
+    bs = [AtomicScattering(s).b() for s in symbols]
+    total_xs = sum(AtomicScattering(s).sigma() for s in symbols)    
+    bovermass = np.array(bs)/np.sqrt(masses)
     #
     import tqdm
     bins = Q_bins, E_bins
@@ -84,18 +90,27 @@ def from_data_dir(
         pols1 = pols[good1, ibr, :, :] # nQ, natoms, 3
         Q_dot_pol = np.sum(np.transpose(pols1, (1,0,2)) * Q_cart, axis=-1).T # nQ, natoms
         # 
-        F = np.sum(exp_Q_dot_d * Q_dot_pol, axis=-1) # nQ      
-        M = np.abs(F)**2 / omega_good # nQ  
+        F = np.sum(exp_Q_dot_d * Q_dot_pol*bovermass, axis=-1) # nQ      
+        M = np.abs(F)**2 / omega_good # nQ
         I1, Qbb, Ebb = np.histogram2d(Qmag_good, omega_good, bins=bins, weights=M)
         I = I + I1
         continue
-    
+    from mcni.utils import conversion
+    I *= conversion.k2e(1.) # canceling unit: hbar^2Q^2/2M/(hbar*omega)
+    # M is |b_d/sqrt(M_d)exp(iQ.d)(Q.e)|^2/omega
+    # At this moment M (and I) is using b in fm. change it to barn
+    I/=100
+
+    # Need to consider the density of the Q points and the volume of the Q space sampled.
+    uc_vol = atoms.get_volume(); ruc_vol = (2*np.pi)**3/uc_vol
+    I*=1./N*4./3*np.pi*max_Q**3/ruc_vol
+    # additional corrections
     from .use_phonopy import _apply_corrections
-    IQEhist = _apply_corrections(I, Qbb, Ebb, N, mass, uc, doshist, T, Ei, max_det_angle)
+    IQEhist = _apply_corrections(I, Qbb, Ebb, N, average_mass, uc, doshist, T, Ei, max_det_angle)
     if include_multiphonon:
         from .use_phonopy import multiphononSQE
-        mphIQE = multiphononSQE(T=T, doshist=doshist, mass=mass, Q_bins=Q_bins, E_bins=E_bins)
-        IQEhist += mphIQE
+        mphIQE = multiphononSQE(T=T, doshist=doshist, mass=average_mass, Q_bins=Q_bins, E_bins=E_bins)
+        return IQEhist, mphIQE * (total_xs/4/np.pi,0)
     return IQEhist
 
 

phonon/powderSQE/use_phonopy.py
@@ -19,7 +19,6 @@ def from_FORCE_CONSTANTS(
         Ei = 300., # meV
         T=300., # kelvin
         doshist=None, # DOS histogram
-        mass = 12, # hack
         supercell = (6,6,1),
         Q_bins = np.arange(0, 11, 0.1), E_bins = np.arange(0, 50, 0.5),
         workdir = None, N=int(1e6),
@@ -60,7 +59,7 @@ def from_FORCE_CONSTANTS(
     Qpoints = np.array([Qx,Qy,Qz]).T
     # in reciprocal units
     hkls = np.dot(Qpoints, uc.lattice.base.T)/(2*np.pi)
-    # run phonopy
+   # run phonopy
     sys.stdout.write("Running phonopy...  "); sys.stdout.flush()
     qs, freqs, pols = onGrid(
         hkls, sc_mat, 
@@ -81,6 +80,8 @@ def from_FORCE_CONSTANTS(
     from phonopy.interface import vasp
     atoms = vasp.read_vasp(poscar)
     positions = atoms.get_scaled_positions()
+    masses = atoms.get_masses()
+    mass = np.mean(masses)
     # Compute
     import tqdm
     bins = Q_bins, E_bins
@@ -109,7 +110,6 @@ def from_FORCE_CONSTANTS(
 
 
 def _apply_corrections(I, Qbb, Ebb, N, mass, uc, doshist, T, Ei, max_det_angle):
-    I *= 1./mass * conversion.k2e(1.)
     Q = (Qbb[1:] + Qbb[:-1])/2
     # correction 1
     ki = conversion.e2k(Ei)
@@ -121,7 +121,8 @@ def _apply_corrections(I, Qbb, Ebb, N, mass, uc, doshist, T, Ei, max_det_angle):
     thermal_factor = 1./2 * (1./np.tanh(E/2*beta) + 1)
     correction_E = thermal_factor * (2*np.pi)**3/v0 / (2*ki*kf)
     correction_Q = 1./Q
-    correction = np.outer(correction_Q, correction_E)
+    # the additional 1/4pi comes from powder average. See notes
+    correction = 1./4/np.pi*np.outer(correction_Q, correction_E)
     I*=correction
     # # Correction 2 - DW
     dos_e = doshist.E if hasattr(doshist, 'E') else doshist.energy
@@ -138,7 +139,6 @@ def _apply_corrections(I, Qbb, Ebb, N, mass, uc, doshist, T, Ei, max_det_angle):
     # first normalize by "event" count and make it density
     dQ = Q[1] - Q[0]
     dE = E[1] - E[0]
-    IQEhist.I /= N
     IQEhist.I /= dQ*dE
     # Simulate "Vanadium" data
     def norm_at(E, Qaxis, Ei):
@@ -148,12 +148,9 @@ def _apply_corrections(I, Qbb, Ebb, N, mass, uc, doshist, T, Ei, max_det_angle):
         kf = conversion.e2k(Ef)
         Qmin = np.abs(ki - kf)
         Qmax = ki + kf
-        Qmiddle = (Qmin + Qmax)/2
-        v_middle = 1./(Qmax-Qmin)
-        slope = v_middle / Qmiddle
         rt = np.zeros(Qaxis.shape)
         in_range = (Qaxis<Qmax) * (Qaxis>Qmin)
-        rt[in_range] = (Qaxis * slope)[in_range]
+        rt[in_range] = (Qaxis *(2*np.pi/ki/kf))[in_range]
         return rt
     norm_hist = IQEhist.copy()
     norm_hist.I[:] = 0
@@ -169,13 +166,12 @@ def _apply_corrections(I, Qbb, Ebb, N, mass, uc, doshist, T, Ei, max_det_angle):
     for iE, (E1, Qmin1, Qmax1) in enumerate(zip(E, DR_Qmin, DR_Qmax)):
         I[Q<Qmin1, iE] = np.nan
         I[Q>Qmax1, iE] = np.nan
-    rt = IQEhist; scale = 8*np.pi; rt.I*=scale; rt.E2*=scale*scale
-    return rt
+    return IQEhist
     #
     if include_multiphonon:
         ## multiphonon
         mpsqe = phonon.sqehist(
-            dos_e, dos_g, Qmin=Q[0], Qmax=Q[-1]+dQ/2., dQ=dQ, T=T, M=mass, N=5, starting_order=2, Emax=E[-1]*3)
+            dos_e, dos_g, Qmin=Q[0], Qmax=Q[-1]+dQ/2., dQ=dQ, T=T, M=mass, N=9, starting_order=2, Emax=E[-1]*3)
         mpsqe1 = interp_sqe(mpsqe, E)
         mpsqe1.I *= scale_multiphonon; mpsqe1.E2 *= scale_multiphonon*scale_multiphonon
     else:

tests/phonon/powderSQE/IDF_Si_TestCase.py
@@ -38,16 +38,17 @@ class TestCase(unittest.TestCase):
         from mccomponents.sample import phonon as mcphonon
         doshist = mcphonon.read_dos.dos_fromidf(os.path.join(datadir, 'DOS')).doshist
         disp = psidf.disp_from_datadir(datadir)
-        IQEhist = psidf.from_data_dir(
+        IQEhist, mphhist = psidf.from_data_dir(
             datadir=datadir,
             disp=disp,
             N = int(1e6),
             Q_bins=np.arange(0, 14, 0.1), E_bins=np.arange(0,90,.5),
-            mass=28., 
             doshist=doshist,
             T=300., Ei=120., max_det_angle=140.,
             include_multiphonon=True,
         )
+        IQEhist = IQEhist + mphhist
+        hh.dump(IQEhist, 'Si-iqe-test2.h5')
         expected = hh.load(os.path.join(here, 'saved_results/Si-all-phonon-Ei_120-T_300-N_1e6.h5'))
         max = np.nanmax(expected.I)
         reldiff = IQEhist-expected
@@ -57,8 +58,6 @@ class TestCase(unittest.TestCase):
         Ntotal = IQEhist.size()
         self.assert_(Ngood*1./Ntotal>.65)
         self.assert_(Nbigdiff*1./Ngood<.10)
-        # hh.dump(IQEhist, 'Si-iqe-test2.h5')
         return
-
 
 if __name__ == '__main__': unittest.main()

tests/phonon/powderSQE/IDF_TestCase.py
@@ -35,17 +35,26 @@ class TestCase(unittest.TestCase):
         doshist = hh.load(os.path.join(datadir, 'exp_DOS.h5'))
         import mcvine.phonon.powderSQE.IDF as psidf
         disp = psidf.disp_from_datadir(datadir)
-        IQEhist = psidf.from_data_dir(
+        IQEhist, mphhist = psidf.from_data_dir(
             datadir=datadir,
             disp=disp,
-            N = int(1e5),
-            Q_bins=np.arange(0, 4, 0.04), E_bins=np.arange(0,30,.2),
-            mass=12., 
+            N = int(1e6),
+            Q_bins=np.arange(0, 23, 0.1), E_bins=np.arange(0,250,1.),
             doshist=doshist,
-            T=300., Ei=30., max_det_angle=60.,
-            include_multiphonon=False,
+            T=300., Ei=300., max_det_angle=140.,
+            include_multiphonon=True,
         )
-        # hh.dump(IQEhist, 'graphite-singlephonon-Ei_30-T_300-IDF.h5')
+        IQEhist += mphhist
+        hh.dump(IQEhist, 'graphite-allphonon-Ei_300-T_300-IDF.h5')
+        expected = hh.load(os.path.join(here, 'saved_results/graphite-allphonon-Ei_300-T_300-IDF.h5'))
+        max = np.nanmax(expected.I)
+        reldiff = IQEhist-expected
+        reldiff.I/=max; reldiff.E2/=max*max
+        Nbigdiff = (np.abs(reldiff.I)>0.03).sum()
+        Ngood = (IQEhist.I==IQEhist.I).sum()
+        Ntotal = IQEhist.size()
+        self.assert_(Ngood*1./Ntotal>.65)
+        self.assert_(Nbigdiff*1./Ngood<.10)
         if plot:
             plt.figure(figsize=(6,3))
             max = np.nanmax(IQEhist.I)