1ed5cf64f7bc7baf0f08b6a1187ee68484210f12
Jiao Lin <jiao.lin@gmail.com>
2020-09-17 22:27:11 -0400

    debugging

--- builders/aws-codebuild/install-deps.sh
+++ builders/aws-codebuild/install-deps.sh
@@ -5,7 +5,6 @@ export PATH=$HOME/mc/bin:$PATH
 conda create -n test python=$PYTHON_VERSION
 source activate test
 conda install pytest awscli
-conda install -c mcvine/label/unstable mcvine
-conda list mcvine
+conda install -c mcvine/label/unstable mcvine-core
 conda list mcvine-core
 conda install tqdm multiphonon periodictable
