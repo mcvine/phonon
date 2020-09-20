ed97a72312fa2a849a4dc11e606831556616bb1c
Jiao Lin <jiao.lin@gmail.com>
2020-09-20 15:46:53 -0400

    phonon.io.readQgridinfo: just use components.sample.idf.Qgridinfo

--- phonon/io.py
+++ phonon/io.py
@@ -1,14 +1,5 @@
 
 def readQgridinfo(path):
-    # read reciprocal basis vectors from Qgridinfo
-    import os
-    lines = open(path).readlines(6)
-    d = {}
-    for line in lines[:6]:
-        exec(line, d)
-        continue
-    reci_basis = [d['b1'], d['b2'], d['b3']]
-    shape = d['n1'], d['n2'], d['n3']
+    from mccomponents.sample.idf.Qgridinfo import read
+    reci_basis, shape = read(path)
     return reci_basis, shape
-
-
